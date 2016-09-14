de9f84107dca89167f3e4970c02df100d742bfd1
BlackI <iain.black@moodys.com>
2016-09-14 10:11:10 +0100

    Tweaked

--- enable-powershell-remote-scripts.bat
+++ enable-powershell-remote-scripts.bat
@@ -1,1 +1,6 @@
-powershell -Command "Set-ExecutionPolicy unrestricted"
+echo ==^> Setting the PowerShell ExecutionPolicy to RemoteSigned
+powershell -Command "Set-ExecutionPolicy UnRestricted -Force" <NUL
+
+if exist %SystemRoot%\SysWOW64\cmd.exe (
+  %SystemRoot%\SysWOW64\cmd.exe /c powershell -Command "Set-ExecutionPolicy UnRestricted -Force" <NUL
+)
--- scripts/enable-winrm.bat
+++ scripts/enable-winrm.bat
@@ -1,4 +1,18 @@
-winrm quickconfig -quiet
+call winrm quickconfig -q
+call winrm quickconfig -transport:http
+call winrm set winrm/config @{MaxTimeoutms="1800000"}
+call winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+call winrm set winrm/config/service @{AllowUnencrypted="true"}
+call winrm set winrm/config/service/auth @{Basic="true"}
+call winrm set winrm/config/client/auth @{Basic="true"}
+call winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
+
+echo ==^> Restarting winrm service
+sc stop winrm
+ping 127.0.0.1
+sc start winrm
+sc config winrm start= auto
+
 netsh advfirewall firewall add rule name="WinRM HTTP Port 5595" dir=in action=allow protocol=TCP localport=5595
 netsh advfirewall firewall add rule name="WinRM HTTPS Port 5596" dir=in action=allow protocol=TCP localport=5596
 powershell -command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned"
